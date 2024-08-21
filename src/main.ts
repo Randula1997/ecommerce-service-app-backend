8b3bd78423773cfec2562c972722b157ad43c50b
Udesh Piyumantha <it21013928@my.sliit.lk>
2024-08-21 19:16:02 +0530

    "Removed CORS configuration and hardcoded port number in bootstrap function"

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*', // Allow requests from this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only specified methods
-    allowedHeaders: ['Content-Type'], // Allow only specified headers
-  });
-  const port = process.env.PORT || 8080;
+  app.enableCors();
+  // const port = process.env.PORT || 8080;
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  await app.listen(8080);
 }
 bootstrap();
